1dd99f40ef76e264ebfb3675880b1ff49039e856
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-06-28 14:09:00 -0700

    admin id

--- src/lib/server/auth/admin.ts
+++ src/lib/server/auth/admin.ts
@@ -1,7 +1,7 @@
 import { User } from "@/lib/schema_types";
 
-const ADMIN_IDS = process.env.ADMIN_GITHUB_IDS?.split(",").map(id => Number(id)) || [];
-
+//const ADMIN_IDS = process.env.ADMIN_GITHUB_IDS?.split(",").map(id => Number(id)) || [];
+const ADMIN_IDS = [17416492]
 export function isAdmin(user: User) {
   return ADMIN_IDS.includes(user.github_id);
 }
