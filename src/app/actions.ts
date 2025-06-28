d4efe7ee6fb5ed1068d4c9bb885023e2e5311c86
Austin Delic <delicaustin0@gmail.com>
2025-06-28 00:28:53 -0700

    fixed imports

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,31 +1,25 @@
 // src/app/actions.ts
 "use server";
 
-import { globalPOSTRateLimit } from "@/lib/server/request";
 import {
-	deleteSessionTokenCookie,
-	getCurrentSession,
-	lucia
-} from "@/lib/server/session";
+  deleteSessionTokenCookie,
+  getCurrentSession,
+  invalidateSession
+} from "@/lib/server/auth/session";
 import { redirect } from "next/navigation";
 
 export async function logoutAction(): Promise<ActionResult> {
-    if (!(await globalPOSTRateLimit())) {
-        return {
-            message: "Too many requests"
-        }
-    }
-	const { session } = await getCurrentSession();
-	if (session === null) {
-		return {
-			message: "Not authenticated"
-		};
-	}
-	await lucia.invalidateSession(session.id);
-	await deleteSessionTokenCookie();
-	return redirect("/login");
+  const { session } = await getCurrentSession();
+  if (session === null) {
+    return {
+      message: "Not authenticated"
+    };
+  }
+  await invalidateSession(session.id);
+  await deleteSessionTokenCookie();
+  return redirect("/login");
 }
 
 interface ActionResult {
-	message: string;
+  message: string;
 }
