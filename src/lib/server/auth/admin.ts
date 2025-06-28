118cfe3ff538c79ed468b81d4fc2cdb63e783dfd
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-06-28 14:19:40 -0700

    balls

--- src/lib/server/auth/admin.ts
+++ src/lib/server/auth/admin.ts
@@ -3,5 +3,6 @@ import { User } from "@/lib/schema_types";
 //const ADMIN_IDS = process.env.ADMIN_GITHUB_IDS?.split(",").map(id => Number(id)) || [];
 const ADMIN_IDS = [17416492]
 export function isAdmin(user: User) {
+  console.log("checking " + user.github_id + " against " + ADMIN_IDS);
   return ADMIN_IDS.includes(user.github_id);
 }
