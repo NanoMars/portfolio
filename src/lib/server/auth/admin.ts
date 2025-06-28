fe86160232bd9c92bacfe691bd1cd3bb6a5e9f90
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-06-28 14:26:04 -0700

    why is it not

--- src/lib/server/auth/admin.ts
+++ src/lib/server/auth/admin.ts
@@ -1,7 +1,7 @@
 import { User } from "@/lib/schema_types";
 
-//const ADMIN_IDS = process.env.ADMIN_GITHUB_IDS?.split(",").map(id => Number(id)) || [];
-const ADMIN_IDS = [17416492]
+const ADMIN_IDS = process.env.ADMIN_GITHUB_IDS?.split(",").map(id => Number(id)) || [];
+
 export function isAdmin(user: User) {
   console.log("checking " + user.github_id + " against " + ADMIN_IDS);
   return ADMIN_IDS.includes(user.github_id);
