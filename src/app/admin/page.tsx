488e4cd5ef6b8ed3c1896f76e489bf61ca153ee3
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-06-28 17:52:27 -0700

    adding input field stuff

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,28 +1,39 @@
 import type { User } from "@/lib/schema_types";
 import { redirect } from "next/navigation";
-import { LogoutButton } from "../components";
+import InputField, { LogoutButton } from "../components";
 import { isAdmin } from "@/lib/server/auth/admin";
 import { getCurrentSession } from "@/lib/server/auth/session";
 import { logoutAction } from "../actions";
+import ProjectsView from "../components/ProjectsView";
+import { Input } from '@headlessui/react'
 
 export default async function Page() {
   const { user: profile } = await getCurrentSession() as { user: User };
-  if (!profile) {
-    redirect("/login");
-  }
-  if (!isAdmin(profile)) {
-    logoutAction();
-    redirect("/");
-  }
+  // if (!profile) {
+  //   redirect("/login");
+  // }
+  // if (!isAdmin(profile)) {
+  //   logoutAction();
+  //   redirect("/");
+  // }
 
-  const image = `https://avatars.githubusercontent.com/u/${profile.github_id}`;
+  //const image = `https://avatars.githubusercontent.com/u/${profile.github_id}`;
 
   return (
     <div className="flex flex-col items-center gap-12 pt-20">
       <h1 className="text-3xl font-bold">[admin]</h1>
       <div className="flex flex-col items-center gap-4">
-        <img src={image} alt="profile" height="100" width="100" className="rounded-full" />
-        <p className="font-semibold">{profile.email}</p>
+        {/* <img src={image} alt="profile" height="100" width="100" className="rounded-full" /> */}
+        {/* <p className="font-semibold">{profile.email}</p> */}
+        <p className="font-black">welcome, {profile.username}</p>
+        <div>
+          <InputField field_name="asdf" field_description="zxcv" />
+          <textarea name="description" data-focus data-hover></textarea>
+          <Input name="logo_link" type="text" />
+          <Input name="header_link" type="text" />
+          <Input name="link" type="text" />
+        </div>
+        <ProjectsView />
         <LogoutButton />
       </div>
     </div>
--- src/app/components.tsx
+++ src/app/components.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { Description, Field, Input, Label } from '@headlessui/react'
 import { logoutAction } from "./actions";
 import { useFormState } from "react-dom";
+import clsx from "clsx";
 
 const initialState = {
 	message: ""
@@ -13,5 +15,22 @@ export function LogoutButton() {
 		<form action={action}>
 			<button>Sign out</button>
 		</form>
+	);
+}
+
+export default function InputField({ fieldName, fieldDescription }: { fieldName: string; fieldDescription: string }) {
+	return (
+		<div className="w-full max-w-md px-4">
+			<Field>
+				<Label className="text-sm/6 font-medium text-white">{fieldName}</Label>
+				<Description className="text-sm/6 text-white/50">{fieldDescription}</Description>
+				<Input
+					className={clsx(
+						'mt-3 block w-full rounded-lg border-none bg-white/5 px-3 py-1.5 text-sm/6 text-white',
+						'focus:not-data-focus:outline-none data-focus:outline-2 data-focus:-outline-offset-2 data-focus:outline-white/25'
+					)}
+				/>
+			</Field>
+		</div>
 	);
 }
