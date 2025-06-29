67e38eb7a023bab1d1851494818b821a01d4849d
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-06-29 16:50:33 -0700

    write introduction

--- src/app/components/greetingCard.tsx
+++ src/app/components/greetingCard.tsx
@@ -1,14 +1,14 @@
 export default function GreetingCard() {
-  const textWidthPercent = 65;
+  const textWidthPercent = 70;
   return (
     <div>
       
       <div className="mt-8 w-fit border-2 border-black p-4 mx-auto">
         <div className="flex flex-row gap-4">
           <div className="flex flex-col gap-2" style={{ width: `${textWidthPercent}%` }}>
-            <h1 className="text-3xl font-bold w-fit">hi, i&#39;m [armand]</h1>
+            <h1 className="text-3xl font-bold w-fit">Hi, I&#39;m [Armand]</h1>
             <p className="font-semibold break-words w-full max-w-full text-justify">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              I’m an 18 y/o builder from Sydney, currently in SF for Hack Club, working on experimental hardware, software, and games. I care about building fast, creative problem solving, and learning new things. I build to learn, iterate, and see what sticks.
             </p>
           </div>
           <div className="flex flex-col gap-2" style={{ width: `${100 - textWidthPercent}%` }}>
