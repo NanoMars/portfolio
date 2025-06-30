a5270fad2e9b534f60edb3f36921c6542c393d87
NanoMars <174164929+NanoMars@users.noreply.github.com>
2025-06-29 20:28:59 -0700

    remove thingy

--- src/app/contact/metadata.ts
+++
@@ -1,4 +0,0 @@
-export const metadata = {
-  title: "Contact | Armand Packham-McGuiness",
-  description: "Get in touch to talk tech, games, software or hardware. Armand is open to creative collaborations and opportunities.",
-};
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,6 @@
  "use client";
 
 import { useState } from "react";
-import { metadata } from './metadata';
 
 export default function ContactPage() {
   const [form, setForm] = useState({ name: "", email: "", message: ""});
